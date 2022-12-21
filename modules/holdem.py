e2e842141e0580eeca1ade2696b8cb37c52ff276
_reoma <94618841+reoma10014@users.noreply.github.com>
2022-12-21 18:57:06 +0900

    card pop and see bet button class

--- modules/holdem.py
+++ modules/holdem.py
@@ -6,6 +6,65 @@ import random
 
 guild_object = discord.Object(id=1024623147124797450)
 
+ingame: list = [
+
+]
+
+
+class my_hand_button(discord.ui.View):
+    def __init__(self, *, hands: list, author_id: int) -> None:
+        super().__init__(timeout=None)
+        self.hands = hands
+        self.author_id = int(author_id)
+
+    @discord.ui.button(label="눌러서 패 보기", style=discord.ButtonStyle.gray)
+    async def hand_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+        if self.author_id == int(interaction.user.id):
+            embed: discord.Embed = discord.Embed( # author
+                title="당신의 패",
+                description=f"{self.hands[0][0]}, {self.hands[0][1]}"
+            )
+        else:
+            embed: discord.Embed = discord.Embed( # other
+                title="당신의 패",
+                description=f"{self.hands[1][0]}, {self.hands[1][1]}"
+            )
+
+        await interaction.response.send_message(
+            embed=embed,
+            ephemeral=True
+        )
+class bet(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="bet", style=discord.ButtonStyle.green)
+    async def bet_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        pass
+class bet_raise(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="raise", style=discord.ButtonStyle.green)
+    async def raise_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        pass
+class fold(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="raise", style=discord.ButtonStyle.red)
+    async def fold_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        pass
+class call(discord.ui.View):
+    def __init__(self, *, hands: list, author_id: int) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="call", style=discord.ButtonStyle.green)
+    async def call_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        pass
+
+
 @dataclass(kw_only=True)
 class Card:
     def __init__(self):
@@ -72,20 +131,13 @@ class Card:
     def shuffle(self) -> None:
         random.shuffle(self.__deck)
 
-    def burning(self) -> None:
-        top_card: str = self.__deck[0]
-        del self.__deck[0]
-        self._droped_cards.append(top_card)
-
     def pop_card(self) -> str:
         pop_card: str = self.__deck[0]
         del self.__deck[0]
-
         self._droped_cards.append(pop_card)
-
         return pop_card
 
-    def return_deck(self) -> list:
+    def get_deck(self) -> list:
         return self.__deck
 
 
@@ -94,40 +146,58 @@ class Holdem(commands.Cog):
         self.bot = bot
 
     @app_commands.command(
-        name="holdem",
-        description="open thread",
+        name="홀덤",
+        description="홀덤 게임을 시작합니다.",
     )
-    @app_commands.choices(command_type=[
-        discord.app_commands.Choice(name="start", value="start"),
-        discord.app_commands.Choice(name="bet", value="bet"),
-        discord.app_commands.Choice(name="fold", value="fold"),
-        discord.app_commands.Choice(name="check", value="check")
-    ])
     async def holdem_command(self,
                              interaction: discord.Interaction,
-                             command_type: app_commands.Choice[str], other_person:discord.User=None) -> None:
+                             other:discord.User=None) -> None:
+
+        #card settings
+        deck:Card = Card()
+        deck.shuffle()
+
+        field_data: dict = {
+            "author_chips": 19,
+            "other_chips": 19,
+            "field_POT": 2,
+            "author_raised": 0,
+            "other_raised": 0
+        }
+
+        hands: list = [[deck.pop_card(), deck.pop_card()], [deck.pop_card(), deck.pop_card()]] # hands[0] = author, hands[1] = other
+
+
+
+        ingame.append({
+            "player": {
+                str(interaction.user.id): { #author data
+                    "hand": hands[0]
+                },
+                str(other.id): { #other data
+                    "hand": hands[1]
+                }
+            }
+        })
+
+
         await interaction.response.send_message(
             embed=discord.Embed(
-                title="Holdem",
-                description="```please check your dm.```",
+                title="게임 시작",
                 color=0x62c1cc
-            )
+            ),
+            view=my_hand_button(hands=hands, author_id=interaction.user.id),
         )
 
-        if command_type.value == "start":
-            #card settings
-            deck:Card = Card()
-            deck.shuffle()
-            author_cards: list = [deck.pop_card(), deck.pop_card()]
-            other_person_cards: list = [deck.pop_card(), deck.pop_card()]
-
-
-            author_dm = await interaction.user.create_dm()
-            await author_dm.send(f"{author_cards[0]}, {author_cards[1]}")
-
-            other_person_dm = await other_person.create_dm()
-            await other_person_dm.send(f"{other_person_cards[0]}, {other_person_cards[1]}")
-
+        while True:
+            await interaction.channel.send(
+                embed=discord.Embed(
+                    title=f"{interaction.user.mention} 님의 베팅 차례입니다.",
+                    description=f"남은칩:{field_data.get('author_chips')}, POT{field_data.get('field_POT')}",
+                    color=0x00FF00
+                ),
+                views=[my_hand_button(hands=hands, author_id=interaction.user.id), ]
+            )
 
 
 async def setup(bot: commands.Bot) -> None:
